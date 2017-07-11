51ce37fa387c2b7a4da4a40f3e6f67d9ad49f373
LorinTeugels <Lorin.Teugels@hotmail.com>
2017-07-11 06:06:49 -0700

    restarting clean env

---
+++ src/protocols/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export TOOLS=../../lib/avro-tools-1.7.7.jar
+java -jar $TOOLS compile protocol avpr/DomServer.avpr 		../
+java -jar $TOOLS compile protocol avpr/SmartFridge.avpr 	../
+java -jar $TOOLS compile protocol avpr/User.avpr 		../
+java -jar $TOOLS compile protocol avpr/Electable.avpr		../
+java -jar $TOOLS compile protocol avpr/Thermostat.avpr 		../
+java -jar $TOOLS compile protocol avpr/Light.avpr 		../
