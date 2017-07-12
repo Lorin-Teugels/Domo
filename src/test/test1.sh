989f351e4cde17a31bd26f10f7de81d418258992
LorinTeugels <Lorin.Teugels@hotmail.com>
2017-07-12 13:26:18 -0700

    woops test needs commit as well

---
+++ src/test/test1.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+. include.sh
+
+function scenario {
+	# start server
+	$JAVA domotics.DomoticsServer &
+	export SERVER=$!
+	sleep 2
+
+	echo Starting fridge1...
+	$JAVA domotics.SmartFridgeClient &
+	export FRIDGE1=$!
+	sleep 3	#required for getting different ip's
+
+	echo Starting fridge2...
+	$JAVA domotics.SmartFridgeClient &
+	export FRIDGE2=$!
+	sleep $SLEEPER
+
+
+	echo Killing server $SERVER...
+	kill -s SIGTERM $SERVER
+
+	sleep $SLEEPER
+
+	# start server
+	echo Starting server again...
+	$JAVA domotics.DomoticsServer &
+	export SERVER=$!
+	sleep $SLEEPER
+
+	echo Killing server $SERVER...
+	kill -s SIGTERM $SERVER
+	sleep $SLEEPER
+
+	echo Killing fridge2 $FRIDGE2...
+	kill -s SIGTERM $FRIDGE2
+	sleep 10
+	
+
+	# start server
+	echo Starting server again...
+	$JAVA domotics.DomoticsServer &
+	export SERVER=$!
+	sleep 7
+
+	echo Stopping
+	kill -s SIGTERM $FRIDGE1 $SERVER
+}
+
+scenario &> test.log
+
+export COND1=`grep "You have won" test.log`
+export COND2=`grep "not a server anymore" test.log`
+if [[ $COND1 == "" || $COND2 == "" ]] ; then
+        exit 1
+fi
+
